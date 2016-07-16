b103ee39f87b74050e657527069c0d74b7733be8
krithikavenkat <krithivenkat95@gmail.com>
2016-07-16 15:07:53 +0530

    finish

---
+++ even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("enter the num");
+scanf("%d,&num");
+if(num%2==0)
+printf("even number");
+else
+printf("odd number");
+getch();
+}
