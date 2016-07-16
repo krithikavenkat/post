5b235a4281b6b040da140b466f99e6431731fb88
krithikavenkat <krithivenkat95@gmail.com>
2016-07-16 15:00:19 +0530

    finish

---
+++ pos.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=10;
+if(a<0)
+{
+printf("negative number");
+}
+else if(a>0)
+{
+printf("positive number")
+}
+else if(a=0)
+{
+printf("zero");
+}
+getch();
+}
